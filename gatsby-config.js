98f896bee91ff2a86d10c525ceea357cab9054d1
Nick <nicklai88@live.com>
2020-02-23 19:12:53 +0800

    v2
    Content update

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,10 +4,7 @@ require(`dotenv`).config({
 
 module.exports = {
   siteMetadata: {
-    // You can overwrite values here that are used for the SEO component
-    // Of course you can also add new values here to query them like usual
-    // See all options: https://github.com/LekoArts/gatsby-themes/blob/master/themes/gatsby-theme-cara/gatsby-config.js
-    siteTitleAlt: `Cara - Gatsby Starter Portfolio`,
+    siteTitleAlt: `Nick Lai's Portfolio`,
   },
   plugins: [
     {
@@ -24,9 +21,9 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Cara - @lekoarts/gatsby-theme-cara`,
-        short_name: `Cara`,
-        description: `Playful and Colorful One-Page portfolio featuring Parallax effects and animations`,
+        name: `Nick Lai's Portfolio`,
+        short_name: `Portfolio`,
+        description: `Nick's self expression of him and his work`,
         start_url: `/`,
         background_color: `#141821`,
         theme_color: `#f6ad55`,
